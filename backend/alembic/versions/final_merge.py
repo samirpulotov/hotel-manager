c4a19ff6593b35a8d99f0f1b3571f4b95761e44e
samirpulotov <samirpulotov.utka@gmail.com>
2025-05-25 21:58:09 +0300

    tumarhomvero

---
+++ backend/alembic/versions/final_merge.py
@@ -0,0 +1,21 @@
+"""final merge
+
+Revision ID: final_merge
+Revises: 3931a93a7a69, 504ce9c33067
+Create Date: 2024-03-19
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'final_merge'
+down_revision = ('3931a93a7a69', '504ce9c33067')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass 
