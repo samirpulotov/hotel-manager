6cfff7b17604018535b75a3ad2a6b897e8023312
samirpulotov <samirpulotov.utka@gmail.com>
2025-05-25 21:34:40 +0300

    tomarehomo

---
+++ backend/alembic/versions/3931a93a7a69_merge_heads.py
@@ -0,0 +1,21 @@
+"""merge heads
+
+Revision ID: 3931a93a7a69
+Revises: 2f042bfbb19e, 3c53388de027
+Create Date: 2024-03-19
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '3931a93a7a69'
+down_revision = ('2f042bfbb19e', '3c53388de027')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass 
